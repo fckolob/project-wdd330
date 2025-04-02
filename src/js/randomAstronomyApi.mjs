f63b3d2a73f8e4d6d32d4fc18b8d4c99ea58aa92
fckolob <131497920+fckolob@users.noreply.github.com>
2025-04-02 18:30:32 -0300

    modified conditions for using the iframe in the random astronomy template

--- src/js/randomAstronomyApi.mjs
+++ src/js/randomAstronomyApi.mjs
@@ -10,11 +10,11 @@ try {
 }}
 
 export function templateStringLiteralAstronomy(data) {
-	if(data.media_type = "video"){
+	if(data.media_type = "video" && !data.url.includes(".jpg")){
 		return  `<div><h1>${data.title}</h1><p>${data.date}</p><p>${data.explanation}</p><iframe id="video" width="420" height="315"
-src="${data.url}">
+src="${data.url}" allowfullscreen>
 </iframe><div/>`;
 	}
 	else{
-	return `<div><h1>${data.title}</h1><p>${data.date}</p><p>${data.explanation}</p><a href="${data.url}"><img class="astronomy-img" src="${data.url}" alt="${data.title}"></img><a/><div/>`;}
+	return `<div><h1>${data.title}</h1><p>${data.date}</p><p>${data.explanation}</p><a href="${data.hdurl}"><img class="astronomy-img" src="${data.url}" alt="${data.title}"></img><a/><div/>`;}
 }
